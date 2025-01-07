7f1d3cb1192f420d25b0038a90bcc31722d91a77
Ilham <irhammadaizzatila@gmail.com>
2025-01-07 19:06:08 +0700

    Ganti Image

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const messages = [
     { 
         text: "You're not rian bro.😄", 
-        image: "https://i.pinimg.com/originals/7e/46/31/7e4631b6fea30649ce7f4f663fa6f501.gif",
+        image: "https://cdn.discordapp.com/attachments/1238397145615437834/1326159349130985492/apa_elo_tega.jpg?ex=677e69a3&is=677d1823&hm=e75353cf0550d1657b70e7296af67bfaf1b602e0a99be954acca5893b52acb73&",
         minValue: 0,
         maxValue: 20
     },
     { 
         text: "Kamu sepertinya gabrian😑 (pangkat terendah dari rian)", 
-        image: "https://media.tenor.com/images/3e9c61c8e2e877f5fe7e4892fe6a4dd5/tenor.gif",
+        image: "https://cdn.discordapp.com/attachments/1238397145615437834/1326159349663404112/dajjal.png?ex=677e69a3&is=677d1823&hm=b30e3ba7e72ed3277187847f010464ddf0b6e875a4902f8599bead976e3dc412&",
         minValue: 21,
         maxValue: 40
     },
     { 
         text: "Kamu sepertinya antek-antek rian 😡", 
-        image: "https://media1.tenor.com/images/98557c9e9c0d42a6363c3e0ed1a6d0dc/tenor.gif",
+        image: "https://cdn.discordapp.com/attachments/1238397145615437834/1326159350120579092/IMG-20231124-WA0030.jpg?ex=677e69a3&is=677d1823&hm=293007be210252e68b942102c2783511c6280138bebc9ee40e5c2ce14cbdc28e&",
         minValue: 41,
         maxValue: 70
     },
     { 
         text: "Kamu rian!😲", 
-        image: "https://media.tenor.com/images/3b3c1c5e9b988c91f1d85fde61fd7151/tenor.gif",
+        image: "https://cdn.discordapp.com/attachments/1238397145615437834/1326159350502395974/279088014_5072505696151983_3829913616711343318_n.jpg?ex=677e69a4&is=677d1824&hm=236414af0365695c42ac6a336509f3548f1607399d4fbe2a5a6349df000d948d&",
         minValue: 71,
         maxValue: 100
     }
